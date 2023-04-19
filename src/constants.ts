b42e06ee1071958ac84b8e3eab21684505f70306
Emma Lejeck <nuck@kitsu.io>
2023-04-18 20:15:13 -0700

    Remove unused events

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,3 @@
 export const ADDON_ID = 'storybook-addon-themes';
 export const TOOL_ID = `${ADDON_ID}/tool`;
 export const PARAM_KEY = 'themes';
-
-export const EVENTS = {
-  CHANGE: `${ADDON_ID}/change`,
-  DECORATOR: `${ADDON_ID}/decorator`,
-  THEME: `${ADDON_ID}/theme`,
-};
